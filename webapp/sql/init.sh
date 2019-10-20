6d5014083d4bccb3e3bf25a44bbe46f51c3cdeb5
yuji314159 <yuji314159@gmail.com>
2019-10-20 15:40:11 +0900

    split init.sh

--- webapp/sql/init.sh
+++ webapp/sql/init.sh
@@ -11,7 +11,10 @@ export MYSQL_PWD=${MYSQL_PASSWORD:-isutrain}
 export LANG="C.UTF-8"
 cd $CURRENT_DIR
 
-cat 01_schema.sql 90_train.sql 91_station.sql 93_seat.sql \
-94_0_train_timetable.sql 94_1_train_timetable.sql 94_2_train_timetable.sql \
-94_3_train_timetable.sql 94_4_train_timetable.sql 94_5_train_timetable.sql \
-| mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 01_schema.sql 90_train.sql 91_station.sql 93_seat.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 94_0_train_timetable.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 94_1_train_timetable.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 94_2_train_timetable.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 94_3_train_timetable.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 94_4_train_timetable.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
+cat 94_5_train_timetable.sql | mysql --defaults-file=/dev/null -h $MYSQL_HOST -P $MYSQL_PORT -u $MYSQL_USER $MYSQL_DBNAME
